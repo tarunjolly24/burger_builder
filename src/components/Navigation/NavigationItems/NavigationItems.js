ab7f96c8bec662ff69439802cf78beae82f4fed8
tarun <tarun.jolly@tothenew.com>
2021-03-30 20:35:16 +0530

    few testing update
    Signed-off-by: tarun <tarun.jolly@tothenew.com>

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -2,6 +2,7 @@ import React from 'react';
 import classes from './NavigationItems.module.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 const navigationItems = (props) => {
+    // console.log(props);
     return( <ul className={classes.NavigationItems}>
         <NavigationItem exact link="/"> Burger Builder </NavigationItem>
         {props.isAuthenticated ?<NavigationItem link="/orders"> Orders </NavigationItem>:null}
@@ -11,6 +12,7 @@ const navigationItems = (props) => {
          :
          <NavigationItem link="/logout"> Logout </NavigationItem>
          }
+         
 
     </ul>);
 }
